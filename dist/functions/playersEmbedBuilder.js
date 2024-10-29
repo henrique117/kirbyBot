671699f2b04f322ece583269cbcf8fb9981dcc55
Henrique <86057591+henrique117@users.noreply.github.com>
2024-10-29 14:24:21 -0300

    Delete dist directory

--- dist/functions/playersEmbedBuilder.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = playerEmbedBuilder;
-const discord_js_1 = require("discord.js");
-async function playerEmbedBuilder(usersVector) {
-    const embedString = usersVector.join('\n') || 'Nenhum registro encontrado!';
-    return new discord_js_1.EmbedBuilder().setColor('Aqua').setTitle('Players achados online:').setDescription(embedString);
-}
